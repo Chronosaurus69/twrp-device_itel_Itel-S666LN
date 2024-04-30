caae8e711657c1cdb9401fc3098f81d176137615
darknius09 <darkniuslightness@gmail.com>
2024-04-30 13:08:15 +0000

    S666LN: Initial TWRP device tree
    Signed-off-by: darknius09 <darkniuslightness@gmail.com>

---
+++ device.mk
@@ -0,0 +1,8 @@
+#
+# Copyright (C) 2024 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from mt6789-common
+$(call inherit-product, device/transsion/mt6789-common/common.mk)
